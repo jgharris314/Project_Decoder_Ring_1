8fd672f2d0285d2561e56b2a452e2cc1860138aa
Grey Elerson <grey.l.elerson@gmail.com>
2021-02-11 10:26:18 -0700

    polybius notes

--- src/polybius.js
+++ src/polybius.js
@@ -1,10 +1,9 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+  
+  // You took the difficult route to a solution. The dreaded 2D array.
+  // I did the same thing, but slightly different implementation. 
+  // Glad I'm not the only one who took the hard way .
   const alphabet = [
     ["a", "b", "c", "d", "e"],
     ["f", "g", "h", "(i/j)", "k"],
@@ -15,46 +14,72 @@ const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
     if (!encode) {
-      // create an array of 'words' ["112211", "212121"]
+
+      // testInput doesn't seem like a GREAT variable name. Could stand to be
+      // more descriptive.
       const testInput = input.split(" ");
       let sentence = [];
-      // itereate through each word
+      
       for (word of testInput) {
-        // if the number of numbers isn't even reject
+        
         if (word.length % 2 != 0) {
           return false;
         } else {
           let newWord = "";
-          //itereate through each pair of number that's why we +=2
+          // DUDE! +=2 !! Idk why that didn't occur to me!
           for (let i = 0; i < word.length; i += 2) {
             let column = word[i];
             let row = word[i + 1];
-            // reverse keys because matrix logic  != reading logic
+            // elegant AF
             newWord += `${alphabet[row - 1][column - 1]}`;
           }
           sentence.push(newWord);
         }
       }
-
+      // Only thing that seems weird here is that `join()` already returns
+      // a string. What exactly are you coercing into string format?
       return sentence.join(" ").toString();
+
     } else {
+
       const lowered = input.toLowerCase();
       let encoded = "";
       
-      // itereate through each letter
+      // Okay, so this is about to take a turn. You seem to have a firm grasp
+      // on process efficiencies, so I think you'll understand what I'm about
+      // to say. It's not shady, so calm down.
+
+      // First off, three nested for loops is too much. With rare exceptions, if
+      // you're using more than two for loops, you're using a jackhammer to carve
+      // an ice sculpture. There's almost always a better way. Think about this:
+      // The first loop goes through the whole message. If it's 25 chars long,
+      // thats 25 loops. The second goes through 5 iterations, and the third goes 
+      // 5 more. That's 25^2... You loop 25 times for every character in the input.
+
+      // If you used an organized `Array.find()`, possibly even a nested `find()`
+      // then you'd save a buttload of process efficiency. /end rant
+
+
+
+      // Definitely better to call the first variable `letter-index`, since it
+      // does not, in fact, hold a letter value
       for (let letter = 0; letter < lowered.length; letter++) {
-        // itereate through each row
+        
         for (let row = 0; row < alphabet.length; row++) {
-          // itereate through each column
+          
           for (let column = 0; column < alphabet[row].length; column++) {
-            // find letter location
+            
             if (lowered[letter] == alphabet[row][column]) {
               //reverse the keys because matrix logic != reading logic
+              //
+              // Another nice catch on your part
               encoded += `${column + 1}${row + 1}`;
             }
           }
           //detect if i or j is there a better way to do this?
         }
+        // "better"? Idk. This is a pretty simple operation, but you could save
+        // some keystrokes with something like 'ij'.includes(lowered[letter])
         if (lowered[letter] == "i" || lowered[letter] == "j") {
           encoded += `42`;
           //   //   //detect if a space and this as well
