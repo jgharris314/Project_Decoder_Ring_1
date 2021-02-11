0e04561ea814326a6cf356393c46ba4b28d96bf5
Grey Elerson <grey.l.elerson@gmail.com>
2021-02-11 10:06:41 -0700

    added caesar notes

--- src/caesar.js
+++ src/caesar.js
@@ -1,23 +1,32 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+
+  // There are those beautiful constants again. I really might yoink this idea.
+  // Furthermore, this is a great place to introduce a concept you've now 
+  // illustrated: Closures. 
+
+  // Do you see how these constants are defined within the `caesarModule` scope,
+  // but outside of the `caesar()` function scope? That means they are global 
+  // from the perspective of the `caesar()` function, but they are private
+  // and protected from anything outside the `caesarModule`. You have ENCLOSED
+  // these constant values, and limited access to them. That's a closure.
+  // https://www.youtube.com/watch?v=GD6qtc2_AQA
+
   const LOWERCASE_A = 97;
   const LOWERCASE_Z = 122;
   const ALPHABET_LENGTH = 26;
 
   function caesar(input, shift, encode = true) {
-    // check if shift is proper value
+
     if (!shift || shift === 0 || shift > 25 || shift < -25){ 
       //so many or's it could be a viking ship
+      //
+      // Your jokes are so cringe, you're gonna be an amazing programmer.
       return false;
     };
-    // convert to lower case
+    
     const newMsg = input.toLowerCase();
-    // check for encode or decode
+    
     if(!encode){
       shift *= -1;
     }
@@ -40,6 +49,11 @@ const caesarModule = (function () {
     }else {
       decoded += String.fromCharCode(tempLtr + shift)
     }
+
+    // Not even gonna lie -- when I saw a bunch of if/else blocks, I prepared
+    // myself to hate that code. But the way you structured it is actually 
+    // super elegant. And your encode vs decode logic is minimal. I dig it.
+
   }
     return decoded;
   }
